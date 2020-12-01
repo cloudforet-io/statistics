77f20178053fb3869db8cf845f074afaee9b5c2b
Jongmin Kim <whdalsrnt@megazone.com>
2020-12-01 22:52:28 +0900

    Change tag search spec
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/statistics/service/schedule_service.py
+++ src/spaceone/statistics/service/schedule_service.py
@@ -175,6 +175,7 @@ class ScheduleService(BaseService):
     @transaction
     @check_required(['query', 'domain_id'])
     @append_query_filter(['domain_id'])
+    @change_tag_filter('tags')
     @append_keyword_filter(['schedule_id', 'topic', 'resource_type'])
     def stat(self, params):
         """
