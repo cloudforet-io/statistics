0d21b44e96a2f496ed55612ce651af3bf2262eb7
Jongmin Kim <whdalsrnt@megazone.com>
2021-01-31 21:35:39 +0900

    Add RBAC features
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- test/service/test_history_service.py
+++ test/service/test_history_service.py
@@ -25,8 +25,6 @@ class TestHistoryService(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         config.init_conf(package='spaceone.statistics')
-        config.set_service_config()
-        config.set_global(DATABASE_SUPPORT_AWS_DOCUMENT_DB=True)
         connect('test', host='mongomock://localhost')
 
         cls.domain_id = utils.generate_id('domain')
