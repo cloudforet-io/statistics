22753ac7b407ddda714d0833a337f7e392afce7e
Jongmin Kim <whdalsrnt@megazone.com>
2022-03-23 17:52:48 +0900

    feat: add service connector's default config
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/statistics/conf/global_conf.py
+++ src/spaceone/statistics/conf/global_conf.py
@@ -23,6 +23,14 @@ HANDLERS = {
 
 CONNECTORS = {
     'ServiceConnector': {
+        'statistics': 'grpc://localhost:50051/v1',
+        'identity': 'grpc://identity:50051/v1',
+        'inventory': 'grpc://inventory:50051/v1',
+        'monitoring': 'grpc://monitoring:50051/v1',
+        'cost_analysis': 'grpc://cost-analysis:50051/v1',
+        'notification': 'grpc://notification:50051/v1',
+        'config': 'grpc://config:50051/v1',
+        'secret': 'grpc://secret:50051/v1',
     }
 }
 
