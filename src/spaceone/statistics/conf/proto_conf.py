58ee02eb59e47b550ef142139ccd5e7651afbfe1
seolmin <seolmin@megazone.com>
2023-12-19 14:26:50 +0900

    refactor change all APIs for SpaceONE 2.0

--- src/spaceone/statistics/conf/proto_conf.py
+++
@@ -1,5 +0,0 @@
-PROTO = {
-    'spaceone.statistics.api.v1.resource': ['Resource'],
-    'spaceone.statistics.api.v1.schedule': ['Schedule'],
-    'spaceone.statistics.api.v1.history': ['History']
-}
--- src/spaceone/statistics/service/schedule_service.py
+++ src/spaceone/statistics/service/schedule_service.py
@@ -2,11 +2,11 @@ import logging
 import copy
 
 from spaceone.core.service import *
-from spaceone.core import utils
 
 from spaceone.statistics.error import *
 from spaceone.statistics.manager.resource_manager import ResourceManager
 from spaceone.statistics.manager.schedule_manager import ScheduleManager
+from spaceone.statistics.model import Schedule
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -16,153 +16,201 @@ _LOGGER = logging.getLogger(__name__)
 @mutation_handler
 @event_handler
 class ScheduleService(BaseService):
+    resource = "Schedule"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.resource_mgr: ResourceManager = self.locator.get_manager('ResourceManager')
-        self.schedule_mgr: ScheduleManager = self.locator.get_manager('ScheduleManager')
+        self.resource_mgr: ResourceManager = self.locator.get_manager("ResourceManager")
+        self.schedule_mgr: ScheduleManager = self.locator.get_manager("ScheduleManager")
 
-    @transaction(append_meta={'authorization.scope': 'DOMAIN'})
-    @check_required(['topic', 'options', 'schedule', 'domain_id'])
-    def add(self, params):
+    @transaction(
+        permission="statistics:Schedule.write",
+        role_types=["DOMAIN_ADMIN"],
+    )
+    @check_required(["topic", "options", "schedule", "domain_id"])
+    def add(self, params: dict) -> Schedule:
         """Add schedule for statistics
 
         Args:
             params (dict): {
-                'topic': 'str',
-                'options': 'dict',
-                'schedule': 'dict',
+                'topic': 'str',      # required
+                'options': 'dict',   # required
+                'schedule': 'dict',  # required
                 'tags': 'dict',
-                'domain_id': 'str'
+                'domain_id': 'str'   # injected from auth (required)
             }
 
         Returns:
             schedule_vo
         """
 
-        domain_id = params['domain_id']
-        options = copy.deepcopy(params['options'])
-        schedule = params['schedule']
+        domain_id = params["domain_id"]
+        options = copy.deepcopy(params["options"])
+        schedule = params["schedule"]
 
         self._check_schedule(schedule)
         self._verify_query_option(options, domain_id)
-        return self.schedule_mgr.add_schedule(params)
+        return self.schedule_mgr.add_schedule(schedule)
 
-    @transaction(append_meta={'authorization.scope': 'DOMAIN'})
-    @check_required(['schedule_id', 'domain_id'])
-    def update(self, params):
+    @transaction(
+        permission="statistics:Schedule.write",
+        role_types=["DOMAIN_ADMIN"],
+    )
+    @check_required(["schedule_id", "domain_id"])
+    def update(self, params: dict) -> Schedule:
         """Update schedule
 
         Args:
             params (dict): {
-                'schedule_id': 'str',
+                'schedule_id': 'str',   # required
                 'schedule': 'dict',
                 'tags': 'dict',
-                'domain_id': 'str'
+                'domain_id': 'str'      # injected from auth (required)
             }
 
         Returns:
             schedule_vo
         """
-        schedule = params.get('schedule')
-
-        self._check_schedule(schedule)
+        self._check_schedule(params.get("schedule"))
 
         return self.schedule_mgr.update_schedule(params)
 
-    @transaction(append_meta={'authorization.scope': 'DOMAIN'})
-    @check_required(['schedule_id', 'domain_id'])
+    @transaction(
+        permission="statistics:Schedule.write",
+        role_types=["DOMAIN_ADMIN"],
+    )
+    @check_required(["schedule_id", "domain_id"])
     def enable(self, params):
         """Enable schedule
 
         Args:
             params (dict): {
-                'schedule_id': 'str',
-                'domain_id': 'str'
+                'schedule_id': 'str',     # required
+                'domain_id': 'str'        # injected from auth (required)
             }
 
         Returns:
             schedule_vo
         """
 
-        domain_id = params['domain_id']
-        schedule_id = params['schedule_id']
+        domain_id = params["domain_id"]
+        schedule_id = params["schedule_id"]
 
         schedule_vo = self.schedule_mgr.get_schedule(schedule_id, domain_id)
-        return self.schedule_mgr.update_schedule_by_vo({'state': 'ENABLED'}, schedule_vo)
+        return self.schedule_mgr.update_schedule_by_vo(
+            {"state": "ENABLED"}, schedule_vo
+        )
 
-    @transaction(append_meta={'authorization.scope': 'DOMAIN'})
-    @check_required(['schedule_id', 'domain_id'])
+    @transaction(
+        permission="statistics:Schedule.write",
+        role_types=["DOMAIN_ADMIN"],
+    )
+    @check_required(["schedule_id", "domain_id"])
     def disable(self, params):
         """Disable schedule
 
         Args:
             params (dict): {
-                'schedule_id': 'str',
-                'domain_id': 'str'
+                'schedule_id': 'str',     # required
+                'domain_id': 'str'        # injected from auth (required)
             }
 
         Returns:
             schedule_vo
         """
 
-        domain_id = params['domain_id']
-        schedule_id = params['schedule_id']
+        domain_id = params["domain_id"]
+        schedule_id = params["schedule_id"]
 
         schedule_vo = self.schedule_mgr.get_schedule(schedule_id, domain_id)
-        return self.schedule_mgr.update_schedule_by_vo({'state': 'DISABLED'}, schedule_vo)
+        return self.schedule_mgr.update_schedule_by_vo(
+            {"state": "DISABLED"}, schedule_vo
+        )
 
-    @transaction(append_meta={'authorization.scope': 'DOMAIN'})
-    @check_required(['schedule_id', 'domain_id'])
+    @transaction(
+        permission="statistics:Schedule.write",
+        role_types=["DOMAIN_ADMIN"],
+    )
+    @check_required(["schedule_id", "domain_id"])
     def delete(self, params):
         """Delete schedule
 
         Args:
             params (dict): {
-                'schedule_id': 'str',
-                'domain_id': 'str'
+                'schedule_id': 'str',     # required
+                'domain_id': 'str'        # injected from auth (required)
             }
 
         Returns:
             None
         """
 
-        self.schedule_mgr.delete_schedule(params['schedule_id'], params['domain_id'])
+        self.schedule_mgr.delete_schedule(params["schedule_id"], params["domain_id"])
 
-    @transaction(append_meta={'authorization.scope': 'DOMAIN'})
-    @check_required(['schedule_id', 'domain_id'])
+    @transaction(
+        permission="statistics:Schedule.read",
+        role_types=["DOMAIN_ADMIN", "WORKSPACE_OWNER", "WORKSPACE_MEMBER"],
+    )
+    @change_value_by_rule("APPEND", "workspace_id", "*")
+    @change_value_by_rule("APPEND", "user_projects", "*")
+    @check_required(["schedule_id", "domain_id"])
     def get(self, params):
         """Get schedule
 
         Args:
             params (dict): {
-                'schedule_id': 'str',
-                'domain_id': 'str',
-                'only': 'list'
+                'schedule_id': 'str',     # required
+                'domain_id': 'str'        # injected from auth (required)
+                'workspace_id': 'str'     # injected from auth
+                'user_projects': 'list'   # injected from auth
             }
 
         Returns:
             schedule_vo
         """
 
-        return self.schedule_mgr.get_schedule(params['schedule_id'], params['domain_id'], params.get('only'))
+        schedule_id = params["schedule_id"]
+        domain_id = params["domain_id"]
+        workspace_id = params.get("workspace_id")
+        user_projects = params.get("user_projects")
 
-    @transaction(append_meta={'authorization.scope': 'DOMAIN'})
-    @check_required(['domain_id'])
-    @append_query_filter(['schedule_id', 'topic', 'state', 'data_source_id', 'resource_type', 'domain_id'])
-    @append_keyword_filter(['schedule_id', 'topic', 'resource_type'])
+        return self.schedule_mgr.get_schedule(
+            schedule_id, domain_id, workspace_id, user_projects
+        )
+
+    @transaction(
+        permission="statistics:Schedule.read",
+        role_types=["DOMAIN_ADMIN", "WORKSPACE_OWNER", "WORKSPACE_MEMBER"],
+    )
+    @change_value_by_rule("APPEND", "workspace_id", "*")
+    @change_value_by_rule("APPEND", "user_projects", "*")
+    @check_required(["domain_id"])
+    @append_query_filter(
+        [
+            "schedule_id",
+            "topic",
+            "state",
+            "data_source_id",
+            "resource_type",
+            "domain_id",
+            "workspace_id",
+            "user_projects",
+        ]
+    )
+    @append_keyword_filter(["schedule_id", "topic", "resource_type", "domain_id"])
     def list(self, params):
-        """ List schedules
+        """List schedules
 
         Args:
             params (dict): {
+                'query': 'dict (spaceone.api.core.v1.Query)'
                 'schedule_id': 'str',
                 'topic': 'str',
                 'state': 'str',
-                'data_source_id': 'str',
                 'resource_type': 'str',
-                'domain_id': 'str',
-                'query': 'dict (spaceone.api.core.v1.Query)'
+                'domain_id': 'str',                            # injected from auth (required)
+                'workspace_id': 'str',                         # injected from auth
+                'user_projects': 'list'                        # injected from auth
             }
 
         Returns:
@@ -170,19 +218,26 @@ class ScheduleService(BaseService):
             total_count
         """
 
-        query = params.get('query', {})
+        query = params.get("query", {})
         return self.schedule_mgr.list_schedules(query)
 
-    @transaction(append_meta={'authorization.scope': 'DOMAIN'})
-    @check_required(['query', 'domain_id'])
-    @append_query_filter(['domain_id'])
-    @append_keyword_filter(['schedule_id', 'topic', 'resource_type'])
+    @transaction(
+        permission="statistics:Schedule.read",
+        role_types=["DOMAIN_ADMIN", "WORKSPACE_OWNER", "WORKSPACE_MEMBER"],
+    )
+    @change_value_by_rule("APPEND", "workspace_id", "*")
+    @change_value_by_rule("APPEND", "user_projects", "*")
+    @check_required(["query", "domain_id"])
+    @append_query_filter(["domain_id", "workspace_id", "user_projects"])
+    @append_keyword_filter(["schedule_id", "topic", "resource_type"])
     def stat(self, params):
         """
         Args:
             params (dict): {
-                'domain_id': 'str',
                 'query': 'dict (spaceone.api.core.v1.StatisticsQuery)'
+                'domain_id': 'str',                                     # injected from auth (required)
+                'workspace_id': 'str',                                  # injected from auth
+                'user_projects': 'list'                                 # injected from auth
             }
 
         Returns:
@@ -190,29 +245,29 @@ class ScheduleService(BaseService):
 
         """
 
-        query = params.get('query', {})
+        query = params.get("query", {})
         return self.schedule_mgr.stat_schedules(query)
 
-    @transaction
+    @transaction(exclude=["authentication", "authorization", "mutation"])
     @append_query_filter([])
     def list_domains(self, params):
-        """ This is used by Scheduler
+        """This is used by Scheduler
         Returns:
             results (list)
             total_count (int)
         """
-        mgr = self.locator.get_manager('ScheduleManager')
-        query = params.get('query', {})
+        mgr = self.locator.get_manager("ScheduleManager")
+        query = params.get("query", {})
         result = mgr.list_domains(query)
         return result
 
     @staticmethod
-    def _check_schedule(schedule):
+    def _check_schedule(schedule: dict) -> None:
         if schedule and len(schedule) > 1:
             raise ERROR_SCHEDULE_OPTION()
 
-    def _verify_query_option(self, options, domain_id):
-        aggregate = options.get('aggregate', [])
-        page = options.get('page', {})
+    def _verify_query_option(self, options: dict, domain_id: str) -> None:
+        aggregate = options.get("aggregate", [])
+        page = options.get("page", {})
 
         self.resource_mgr.stat(aggregate, page, domain_id)
