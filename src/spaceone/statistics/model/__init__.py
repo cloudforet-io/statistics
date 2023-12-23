0381ad8705bf0cf0d14f0015a0979bb2feef1619
Jongmin Kim <whdalsrnt@megazone.com>
2023-12-23 16:19:10 +0900

    fix: fix import bug

--- src/spaceone/statistics/model/__init__.py
+++ src/spaceone/statistics/model/__init__.py
@@ -1,2 +1,2 @@
-from spaceone.statistics.model.schedule_model import Schedule
-from spaceone.statistics.model.history_model import History
+# from spaceone.statistics.model.schedule_model import Schedule
+# from spaceone.statistics.model.history_model import History
